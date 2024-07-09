4c53956cc735e8bcd1168b6d4b524ee17d80e627
vladimir <128848265+arhixia@users.noreply.github.com>
2024-07-09 19:23:44 +0500

    Update main.py

--- back/main.py
+++ back/main.py
@@ -39,7 +39,7 @@ def get_db():
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-SECRET_KEY = "your_secret_key"
+SECRET_KEY = "..."
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
